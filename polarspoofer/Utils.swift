836a0bbafa4249d7574e51eb0def82ac38ed14aa
Adam Kuczyński <ak@niekoniecznie.pl>
2016-06-06 02:13:51 +0100

    Correct everything

--- polarspoofer/Utils.swift
+++ polarspoofer/Utils.swift
@@ -39,12 +39,13 @@ func readDirectory(path: String) -> [UInt8] {
     for file in try! manager.contentsOfDirectoryAtPath(path) {
         let attr = try! manager.attributesOfItemAtPath(path + file)
         let type = attr[NSFileType]! as! String
-        let size = attr[NSFileSize]! as! Int
         let created = attr[NSFileCreationDate]! as! NSDate
         let modified = attr[NSFileModificationDate]! as! NSDate
+        var size = attr[NSFileSize]! as! Int
         var name = file
         
         if type == NSFileTypeDirectory {
+            size = 0
             name += "/"
         }
         
@@ -64,18 +65,18 @@ func knownDate(from: NSDate) -> DateTime {
     let c = NSCalendar.currentCalendar().components([.Year, .Month, .Day, .Hour, .Minute, .Second], fromDate: from)
     let date = try! Date.Builder().setYear(UInt32(c.year)).setMonth(UInt32(c.month)).setDay(UInt32(c.day)).build()
     let time = try! Time.Builder().setHour(UInt32(c.hour)).setMinute(UInt32(c.minute)).setSecond(UInt32(c.second)).setMilisecond(0).build()
-    return try! DateTime.Builder().setDate(date).setTime(time).build()
+    return try! DateTime.Builder().setDate(date).setTime(time).setTimezone(1).build()
 }
 
 func unknownDate() -> DateTime {
     let date = try! Date.Builder().setYear(1980).setMonth(0).setDay(0).build()
     let time = try! Time.Builder().setHour(0).setMinute(0).setSecond(0).setMilisecond(0).build()
-    return try! DateTime.Builder().setDate(date).setTime(time).build()
+    return try! DateTime.Builder().setDate(date).setTime(time).setTimezone(1).build()
 }
 
 func decode(message: [UInt8]) -> [UInt8] {
     var i = 0, j = 0
-    return [] + message.filter { _ in return i++ % 20 != 0 }.filter { _ in return j++ % 300 != 0 }.dropFirst(2).dropLast()
+    return [] + message.filter { _ in return i++ % 20 != 0 }.filter { _ in return j++ % 304 != 0 }.dropFirst(2).dropLast()
 }
 
 func encode(message: [UInt8]) -> [UInt8] {
